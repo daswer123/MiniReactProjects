ca2cc2f6be62ac0aa79ccc7e8142f272790c9278
daswer123 <daswerq123@gmail.com>
2020-10-13 22:13:08 +0300

    Init weather app

---
+++ weatherapp/src/components/app/index.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import WeatherInfo from "../weatherInfo";
+import WeatherSearch from "../weatherSearch";
+
+const App = () => {
+    const [weatherData,setWeatherData] = useState("");
+
+    <WeatherInfo/>
+}
+
+export default App;
